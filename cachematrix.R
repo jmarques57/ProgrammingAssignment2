fbd02c7c4a47ecfc197477f8c632d0c12c412dc7
Joaquin Marques <joaquin.marques@gmail.com>
2016-09-03 15:07:01 -0400

    assignment 2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
-## The following function returns the inverse of a matrix x,
-## but it first checks if the inverse matrix has been previously
-## calculated and cashed. If it has, it returns the cashed value.
-## If it hasn't, it calculates the inverse matrix, and it cashes 
-## its value before returning it
+## The following acts as a wrapper for all the setters and getters
+## needed to set and get values for both atrix x and its inverse m
+## and to do the initial setting of inverse matrix m to NULL
 
 makeCacheMatrix <- function(x = matrix()) {
+    # initialize inverse matrix m to NULL
 		m <- NULL
+		# declare all set and get functions for both matrix x and its inverse m
 		set <- function(y) {
 				x <<- y
 				m <<- NULL
@@ -13,28 +13,36 @@ makeCacheMatrix <- function(x = matrix()) {
 		get <- function() x
 		setinverse <- function(solve) m <<- solve
 		getinverse <- function() m
+		# list setter and getter functions
 		list(set=set, get=get,
 				setinverse=setinverse,
 				getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following functions return the inverse of a matrix x,
+## but it first checks if the inverse matrix has been previously
+## calculated and cashed. If it has, it returns the cashed value.
+## If it hasn't, it calculates the inverse matrix, and it cashes 
+## its value before returning it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinverse()
+        ## return inverse m if it is not NULL
         if(!is.null(m)) {
         	  message("getting cached data")
             return(m)	
         }
+        ## if inverse m is NULL, calculate its value from x,
+        ## set the cashed value to it, and return it
         data <- x$get()
         m <- solve(data, ...)
         x$setinverse(m)
         m
 }
 
-# Example 1
+# Test Script
 m1=rbind(c(1, -1/4), c(-1/4, 1))
 m1
 i1 <- solve(m1)
@@ -42,7 +50,6 @@ i1
 m2 <- i1%*%m1
 m2
 
-# Example 2
 x <- makeCacheMatrix(m1)
 cacheSolve(x)
 # this last value is identical to m2 above
